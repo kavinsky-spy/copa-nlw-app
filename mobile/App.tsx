2d540cc6ccb12cbaa98c51bfaaad3c3ae140b1ea
julio <julio@zoocha.com>
2023-02-24 07:37:34 -0300

    Changes to be committed: 	modified:   mobile/App.tsx 	modified:   mobile/app.json 	modified:   mobile/package-lock.json 	modified:   mobile/package.json

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, VStack } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <VStack flex={1} bgColor="fuchsia.400" alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </VStack>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: "#FFF",
+    fontSize: 24
+  }
 });
